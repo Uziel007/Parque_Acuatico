d5f14c6ee97f714440e8ec817cf3043f92fc2e5f
Uziel <uzielsanchez007@gmail.com>
2025-03-25 20:14:39 -0600

    Subiendo carpetas de controladores y modelos

---
+++ ParqueAcuaticoWeb/src/main/java/com/example/parqueacuatico/ParqueAcuaticoWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.parqueacuatico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParqueAcuaticoWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParqueAcuaticoWebApplication.class, args);
+	}
+
+}
