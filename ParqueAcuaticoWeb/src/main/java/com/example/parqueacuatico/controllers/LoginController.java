7bc8e0311e72f493e0ce1cbb8940e5d65d52958d
Uziel007 <uzielsanchez007@gmail.com>
2025-03-29 02:14:42 -0600

    Se realizo navegacion entre paginas y diseños

---
+++ ParqueAcuaticoWeb/src/main/java/com/example/parqueacuatico/controllers/LoginController.java
@@ -0,0 +1,36 @@
+package com.example.parqueacuatico.controllers;
+
+import com.example.parqueacuatico.models.Usuario;
+import com.example.parqueacuatico.repositories.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
+
+@Controller
+public class LoginController {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @GetMapping("/login")
+    public String mostrarLogin() {
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String procesarLogin(@RequestParam String email, @RequestParam String password, Model model) {
+        Optional<Usuario> usuario = usuarioRepository.findByEmail(email);
+
+        if (usuario.isPresent() && usuario.get().getPassword().equals(password)) {
+            return "redirect:/compra";
+        } else {
+            model.addAttribute("error", "Credenciales incorrectas. Inténtalo de nuevo.");
+            return "login";
+        }
+    }
+}
---
+++ ParqueAcuaticoWeb/src/main/java/com/example/parqueacuatico/repositories/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package com.example.parqueacuatico.repositories;
+
+import com.example.parqueacuatico.models.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+    Optional<Usuario> findByEmail(String email);
+}
